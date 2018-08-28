5685fa425c27615585ce71c24b3b762ccbe38b1a
dr-JT <tsuky19@gmail.com>
2018-08-28 12:49:18 -0400

    remove for loop in place of group_by()

--- R/pupil.interpolate.R
+++ R/pupil.interpolate.R
@@ -11,51 +11,56 @@
 #' pupil.interpolate(x, type = "cubic-spline", eye.recorded = "both")
 
 pupil.interpolate <- function(x, type = "cubic-spline", eye.recorded = ""){
-  data.list <- data.frame()
-  for (i in unique(x$Trial)[which(!is.na(unique(x$Trial)))]){
-    ## Reduce baseline pupil data
-    data <- dplyr::filter(x, Trial==i)
-
-    ## Preprocessing steps
-    if (eye.recorded=="both"){
+  if (eye.recorded=="both"){
+    data <- dplyr::mutate(data,
+                          L_Pupil_Diameter.mm = ifelse((L_Event=="Blink"|is.na(L_Event)), NA, L_Pupil_Diameter.mm),
+                          R_Pupil_Diameter.mm = ifelse((R_Event=="Blink"|is.na(R_Event)), NA, R_Pupil_Diameter.mm))
+    if (type=="cubic-spline"){
+      data <- dplyr::group_by(data, Trial)
       data <- dplyr::mutate(data,
-                            L_Pupil_Diameter.mm = ifelse((L_Event=="Blink"|is.na(L_Event)), NA, L_Pupil_Diameter.mm),
-                            R_Pupil_Diameter.mm = ifelse((R_Event=="Blink"|is.na(R_Event)), NA, R_Pupil_Diameter.mm))
-      if (type == "cubic-spline") {
-        data <- dplyr::mutate(data,
-                              L_Pupil_Diameter.mm = zoo::na.spline(L_Pupil_Diameter.mm, na.rm = TRUE),
-                              R_Pupil_Diameter.mm = zoo::na.spline(R_Pupil_Diameter.mm, na.rm = TRUE))
-      } else if (type == "linear") {
-        data <- dplyr::mutate(data,
-                              L_Pupil_Diameter.mm = zoo::na.approx(L_Pupil_Diameter.mm, na.rm = TRUE),
-                              R_Pupil_Diameter.mm = zoo::na.approx(R_Pupil_Diameter.mm, na.rm = TRUE))
-      }
-
-    } else if (eye.recorded=="left"){
+                            index_left = ifelse(is.na(L_Pupil_Diameter.mm), NA, row_number()),
+                            index_left = zoo::na.approx(index_left, na.rm = FALSE),
+                            index_right = ifelse(is.na(R_Pupil_Diameter.mm), NA, row_number()),
+                            index_right = zoo::na.approx(index_right, na.rm = FALSE),
+                            L_Pupil_Diameter.mm = zoo::na.spline(L_Pupil_Diameter.mm, na.rm = FALSE, x = index_left),
+                            R_Pupil_Diameter.mm = zoo::na.spline(R_Pupil_Diameter.mm, na.rm = FALSE, x = index_right))
+      data <- dplyr::select(data, -index_left, -index_right)
+    } else if (type=="linear"){
       data <- dplyr::mutate(data,
-                            L_Pupil_Diameter.mm = ifelse((L_Event=="Blink"|is.na(L_Event)), NA, L_Pupil_Diameter.mm),
-                            Eye = "Left")
-      if (type == "cubic-spline") {
-        data <- dplyr::mutate(data,
-                              L_Pupil_Diameter.mm = zoo::na.spline(L_Pupil_Diameter.mm, na.rm = TRUE))
-      } else if (type == "linear") {
-        data <- dplyr::mutate(data,
-                              L_Pupil_Diameter.mm = zoo::na.approx(L_Pupil_Diameter.mm, na.rm = TRUE))
-      }
-
-    } else if (eye.recorded=="right"){
-      data <- dplyr::mutate(data,
-                            R_Pupil_Diameter.mm = ifelse((R_Event=="Blink"|is.na(R_Event)), NA, R_Pupil_Diameter.mm),
-                            Eye = "Right")
-      if (type == "cubic-spline") {
-        data <- dplyr::mutate(data,
-                              R_Pupil_Diameter.mm = zoo::na.spline(R_Pupil_Diameter.mm, na.rm = TRUE))
-      } else if (type == "linear") {
-        data <- dplyr::mutate(data,
-                              R_Pupil_Diameter.mm = zoo::na.approx(R_Pupil_Diameter.mm, na.rm = TRUE))
-      }
+                            L_Pupil_Diameter.mm = zoo::na.approx(L_Pupil_Diameter.mm, na.rm = FALSE),
+                            R_Pupil_Diameter.mm = zoo::na.approx(R_Pupil_Diameter.mm, na.rm = FALSE))
     }
-    data.list <- rbind(data.list,data.frame(data))
+
+  } else if (eye.recorded=="left"){
+    data <- dplyr::mutate(data,
+                          L_Pupil_Diameter.mm = ifelse((L_Event=="Blink"|is.na(L_Event)), NA, L_Pupil_Diameter.mm))
+    if (type=="cubic-spline"){
+      data <- dplyr::group_by(data, Trial)
+      data <- dplyr::mutate(data,
+                            index_left = ifelse(is.na(L_Pupil_Diameter.mm), NA, row_number()),
+                            index_left = zoo::na.approx(index_left, na.rm = FALSE),
+                            L_Pupil_Diameter.mm = zoo::na.spline(L_Pupil_Diameter.mm, na.rm = FALSE, x = index_left))
+      data <- dplyr::select(data, -index_left)
+    } else if (type=="linear"){
+      data <- dplyr::mutate(data,
+                            L_Pupil_Diameter.mm = zoo::na.approx(L_Pupil_Diameter.mm, na.rm = FALSE))
+    }
+
+  } else if (eye.recorded=="right"){
+    data <- dplyr::mutate(data,
+                          R_Pupil_Diameter.mm = ifelse((R_Event=="Blink"|is.na(R_Event)), NA, R_Pupil_Diameter.mm))
+    if (type=="cubic-spline"){
+      data <- dplyr::group_by(data, Trial)
+      data <- dplyr::mutate(data,
+                            index_right = ifelse(is.na(R_Pupil_Diameter.mm), NA, row_number()),
+                            index_right = zoo::na.approx(index_right, na.rm = FALSE),
+                            R_Pupil_Diameter.mm = zoo::na.spline(R_Pupil_Diameter.mm, na.rm = FALSE, x = index_right))
+      data <- dplyr::select(data, -index_right)
+    } else if (type=="linear"){
+      data <- dplyr::mutate(data,
+                            R_Pupil_Diameter.mm = zoo::na.approx(R_Pupil_Diameter.mm, na.rm = FALSE))
+    }
+
   }
-  return(data.list)
+  return(data)
 }
