3a43c98e52c0b67431b5357fd376f0d80b22cf75
Jason Tsukahara <tsuky19@gmail.com>
2018-09-09 19:50:39 -0400

    create pupil.cor()

---
+++ R/pupil.cor.R
@@ -0,0 +1,16 @@
+#' Preprocessnig Function
+#'
+#' This function is for preprocessing baseline pupil data. It takes as input a data file that has been processed
+#' with the .convert() functions. The output of this function is a preprocessed data file. Preprocessing steps can
+#' include interpolation and smoothing
+#' @param x dataframe
+#' @keywords eye
+#' @export
+#' @examples
+#' pupil.cor(x)
+
+pupil.cor <- function(x){
+  x <- dplyr::mutate(x, Pupils.r = stats::cor(L_Pupil_Diameter.mm, R_Pupil_Diameter.mm, use = "pairwise.complete.obs"),
+                     Pupils.r = ifelse(is.na(pupils.cor), 0, Pupils.r))
+  return(x)
+}
