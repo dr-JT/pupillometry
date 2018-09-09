4b1528e90fee58775888a5f0b06176c07e0a6e41
Jason Tsukahara <tsuky19@gmail.com>
2018-09-09 19:51:06 -0400

    remove eye.recorded parameterr

--- R/pupil.interpolate.R
+++ R/pupil.interpolate.R
@@ -6,13 +6,12 @@
 #' @param type What type of interpolation to use? (default: cubic-spline)
 #' @param maxgap Maximum number of NAs to interpolate over. Anything gaps over this value will not be interpolated.
 #' @param hz The recording frequency (used to calculate window size)
-#' @param eye.recorded Is there pupil x for the left, right, or both eyes?
 #' @keywords interpolate
 #' @export
 #' @examples
 #' pupil.interpolate(x, type = "cubic-spline", eye.recorded = "both")
 
-pupil.interpolate <- function(x, type = "cubic-spline", maxgap = Inf, hz = "", eye.recorded = ""){
+pupil.interpolate <- function(x, type = "cubic-spline", maxgap = Inf, hz = ""){
   if (maxgap!=Inf){
     if (hz==""){
       hz <- x$Hz[1]
@@ -20,53 +19,17 @@ pupil.interpolate <- function(x, type = "cubic-spline", maxgap = Inf, hz = "", e
     maxgap <- maxgap/(hz/1000)
   }
   x <- dplyr::group_by(x, Trial)
-  if (eye.recorded=="both"){
-    if (type=="cubic-spline"){
-      x <- dplyr::mutate(x,
-                         index_left = ifelse(is.na(L_Pupil_Diameter.mm), NA, dplyr::row_number()),
-                         index_left = zoo::na.approx(index_left, na.rm = FALSE),
-                         index_right = ifelse(is.na(R_Pupil_Diameter.mm), NA, dplyr::row_number()),
-                         index_right = zoo::na.approx(index_right, na.rm = FALSE),
-                         L_Pupil_Diameter.mm = ifelse(L_Missing.Total<1,
-                                                      zoo::na.spline(L_Pupil_Diameter.mm, na.rm = FALSE, x = index_left, maxgap = maxgap),
-                                                      NA),
-                         R_Pupil_Diameter.mm = ifelse(R_Missing.Total<1,
-                                                      zoo::na.spline(R_Pupil_Diameter.mm, na.rm = FALSE, x = index_right, maxgap = maxgap),
-                                                      NA))
-      x <- dplyr::select(x, -index_left, -index_right)
-    } else if (type=="linear"){
-      x <- dplyr::mutate(x,
-                         L_Pupil_Diameter.mm = zoo::na.approx(L_Pupil_Diameter.mm, na.rm = FALSE, maxgap = maxgap),
-                         R_Pupil_Diameter.mm = zoo::na.approx(R_Pupil_Diameter.mm, na.rm = FALSE, maxgap = maxgap))
-    }
-
-  } else if (eye.recorded=="left"){
-    if (type=="cubic-spline"){
-      x <- dplyr::mutate(x,
-                         index_left = ifelse(is.na(L_Pupil_Diameter.mm), NA, dplyr::row_number()),
-                         index_left = zoo::na.approx(index_left, na.rm = FALSE),
-                         L_Pupil_Diameter.mm = ifelse(L_Missing.Total<1,
-                                                      zoo::na.spline(L_Pupil_Diameter.mm, na.rm = FALSE, x = index_left, maxgap = maxgap),
-                                                      NA))
-      x <- dplyr::select(x, -index_left)
-    } else if (type=="linear"){
-      x <- dplyr::mutate(x,
-                         L_Pupil_Diameter.mm = zoo::na.approx(L_Pupil_Diameter.mm, na.rm = FALSE, maxgap = maxgap))
-    }
-
-  } else if (eye.recorded=="right"){
-    if (type=="cubic-spline"){
-      x <- dplyr::mutate(x,
-                         index_right = ifelse(is.na(R_Pupil_Diameter.mm), NA, dplyr::row_number()),
-                         index_right = zoo::na.approx(index_right, na.rm = FALSE),
-                         R_Pupil_Diameter.mm = ifelse(R_Missing.Total<1,
-                                                      zoo::na.spline(R_Pupil_Diameter.mm, na.rm = FALSE, x = index_right, maxgap = maxgap),
-                                                      NA))
-      x <- dplyr::select(x, -index_right)
-    } else if (type=="linear"){
-      x <- dplyr::mutate(x,
-                         R_Pupil_Diameter.mm = zoo::na.approx(R_Pupil_Diameter.mm, na.rm = FALSE, maxgap = maxgap))
-    }
+  if (type=="cubic-spline"){
+    x <- dplyr::mutate(x,
+                       index = ifelse(is.na(Pupil_Diameter.mm), NA, dplyr::row_number()),
+                       index = zoo::na.approx(index, na.rm = FALSE),
+                       Pupil_Diameter.mm = ifelse(Missing.Total<1,
+                                                  zoo::na.spline(Pupil_Diameter.mm, na.rm = FALSE, x = index, maxgap = maxgap),
+                                                  NA))
+    x <- dplyr::select(x, -index)
+  } else if (type=="linear"){
+    x <- dplyr::mutate(x,
+                       Pupil_Diameter.mm = zoo::na.approx(Pupil_Diameter.mm, na.rm = FALSE, maxgap = maxgap))
   }
   x <- dplyr::ungroup(x)
   return(x)
--- R/pupil.smooth.R
+++ R/pupil.smooth.R
@@ -6,49 +6,27 @@
 #' @param type The type of smoothing function to apply. Hann or moving window average
 #' @param window The size of the smoothing window (default = 5). Value is in milliseconds
 #' @param hz The recording frequency (used to calculate window size)
-#' @param eye.recorded Is there pupil x for the left, right, or both eyes?
 #' @keywords smooth
 #' @export
 #' @examples
 #' pupil.smooth(x, type = "hann", window = 11, eye.recorded = "both")
 
-pupil.smooth <- function(x, type = "hann", window = 5, hz = "", eye.recorded = ""){
+pupil.smooth <- function(x, type = "hann", window = 5, hz = ""){
   if (hz==""){
     hz <- x$Hz[1]
   }
   window <- window/(hz/1000)
   x <- dplyr::group_by(x, Trial)
-  if (eye.recorded=="both"){
-    if (type=="hann"){
-      x <- dplyr::mutate(x,
-                         L_Pupil_Diameter.mm = dplR::hanning(L_Pupil_Diameter.mm, n = window),
-                         R_Pupil_Diameter.mm = dplR::hanning(R_Pupil_Diameter.mm, n = window))
-    } else if (type=="mwa"){
-      x <- dplyr::mutate(x,
-                         L_Pupil_Diameter.mm = zoo::rollapply(L_Pupil_Diameter.mm, width = window, FUN = mean, partial = TRUE, na.rm = TRUE),
-                         L_Pupil_Diameter.mm = ifelse(is.na(L_Pupil_Diameter.mm),NA,L_Pupil_Diameter.mm),
-                         R_Pupil_Diameter.mm = zoo::rollapply(R_Pupil_Diameter.mm, width = window, FUN = mean, partial = TRUE, na.rm = TRUE),
-                         R_Pupil_Diameter.mm = ifelse(is.na(R_Pupil_Diameter.mm),NA,R_Pupil_Diameter.mm))
-    }
-
-  } else if (eye.recorded=="left"){
-    if (type=="hann"){
-      x <- dplyr::mutate(x, dplR::hanning(L_Pupil_Diameter.mm, n = window))
-    } else if (type=="mwa"){
-      x <- dplyr::mutate(x,
-                         Pupil_Diameter.mm = zoo::rollapply(L_Pupil_Diameter.mm, width = window, FUN = mean, partial = TRUE, na.rm = TRUE),
-                         Pupil_Diameter.mm = ifelse(is.na(L_Pupil_Diameter.mm),NA,L_Pupil_Diameter.mm))
-    }
-
-  } else if (eye.recorded=="right"){
-    if (type=="hann"){
-      x <- dplyr::mutate(x, R_Pupil_Diameter.mm = dplR::hanning(R_Pupil_Diameter.mm, n = window))
-    } else if (type=="mwa"){
-      x <- dplyr::mutate(x,
-                         R_Pupil_Diameter.mm = zoo::rollapply(R_Pupil_Diameter.mm, width = window, FUN = mean, partial = TRUE, na.rm = TRUE),
-                         R_Pupil_Diameter.mm = ifelse(is.na(R_Pupil_Diameter.mm),NA,R_Pupil_Diameter.mm))
-    }
-
+  if (type=="hann"){
+    x <- dplyr::mutate(x, Pupil_Diameter.mm = dplR::hanning(Pupil_Diameter.mm, n = window))
+  } else if (type=="mwa"){
+    x <- dplyr::mutate(x,
+                       Pupil_Diameter.mm = zoo::rollapply(Pupil_Diameter.mm,
+                                                          width = window,
+                                                          FUN = mean,
+                                                          partial = TRUE,
+                                                          na.rm = TRUE),
+                       Pupil_Diameter.mm = ifelse(is.na(Pupil_Diameter.mm),NA,Pupil_Diameter.mm))
   }
   x <- dplyr::ungroup(x)
   return(x)
