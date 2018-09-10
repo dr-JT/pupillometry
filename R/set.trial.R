38e494d0aa6e701652bf56f66fd08b228090e940
dr-JT <tsuky19@gmail.com>
2018-09-10 14:48:14 -0400

    add set.trial()

---
+++ R/set.trial.R
@@ -0,0 +1,18 @@
+#' Pupil Data Function
+#'
+#' This sets the trials relative to a message string in the data
+#' @param x dataframe
+#' @param startrecroding.message Message string that marks the start of a recorind
+#' @keywords set timing
+#' @export
+#' @examples
+#' set.timing(x, start.trial = "Fixation", ms.conversion = 1000)
+
+set.timing <- function(x, startrecroding.message = ""){
+  x <- dplyr::mutate(x,
+                     startrecording.time = ifelse(Message==startrecroding.message, Time, NA),
+                     startrecording.time = ifelse(startrecording.time!=min(startrecording.time, na.rm = TRUE), NA, startrecording.time),
+                     startrecording.time = zoo::na.locf(startrecording.time),
+                     Trial = dplyr::dense_rank(startrecording.time))
+  return(x)
+}
