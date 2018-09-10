ae2fb761d45e9a4ef00069f116be23aa0f6df067
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2018-09-10 17:42:39 -0400

    fix

--- R/set.timing.R
+++ R/set.timing.R
@@ -15,7 +15,12 @@ set.timing <- function(x, trialonset.message = "", ms.conversion = 1, pretrial.d
   x <- dplyr::group_by(x, Trial)
   x <- dplyr::mutate(x,
                      trialonset.time = ifelse(Message==trialonset.message, Time, NA),
-                     min = min(trialonset.time, na.rm = TRUE))
+                     min = min(trialonset.time, na.rm = TRUE),
+                     trialonset.time = ifelse(is.na(trialonset.time) | trialonset.time!=min,NA,trialonset.time),
+                     trialonset.time = zoo::na.locf(trialonset.time),
+                     Time = (Time - trialstart.time)/ms.conversion,
+                     PreTrial = ifelse(Time>=pretrial.duration & Time<0,1,ifelse(Time>=0,0,NA)))
   x <- dplyr::ungroup(x)
+  x <- dplyr::select(x, -trialonset.time)
   return(x)
 }
