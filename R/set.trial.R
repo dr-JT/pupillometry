df9267e649eb4eb2d010e7cd2c964fd31abb06fc
Jason Tsukahara <32802181+dr-JT@users.noreply.github.com>
2018-09-10 17:03:25 -0400

    Fix type

--- R/set.trial.R
+++ R/set.trial.R
@@ -2,15 +2,15 @@
 #'
 #' This sets the trials relative to a message string in the data
 #' @param x dataframe
-#' @param startrecroding.message Message string that marks the start of a recorind
+#' @param startrecording.message Message string that marks the start of a recorind
 #' @keywords set timing
 #' @export
 #' @examples
 #' set.trial(x, start.trial = "Fixation", ms.conversion = 1000)
 
-set.trial <- function(x, startrecroding.message = ""){
+set.trial <- function(x, startrecording.message = ""){
   x <- dplyr::mutate(x,
-                     startrecording.time = ifelse(Message==startrecroding.message, Time, NA),
+                     startrecording.time = ifelse(Message==startrecording.message, Time, NA),
                      startrecording.time = zoo::na.locf(startrecording.time),
                      Trial = dplyr::dense_rank(startrecording.time))
   return(x)
