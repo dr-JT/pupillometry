56e070e0ced576dbd9a58f0b40a8c342cb829e75
dr-JT <tsuky19@gmail.com>
2018-09-10 21:01:15 -0400

    fix

--- R/pupil.downsample.R
+++ R/pupil.downsample.R
@@ -9,14 +9,14 @@
 #' pupil.downsample(x, bin.length = 100)
 
 pupil.downsample <- function(x, bin.length = ""){
-  x <- dplyr::group_by(x, Subject, Trial)
+  x <- dplyr::group_by(x, Trial)
   x <- dplyr::mutate(x, TimeBin = ifelse(min(Time)>=0, Time - min(Time), Time),
                      TimeBin = trunc((Time - min(Time))/bin.length),
                      Time = TimeBin*bin.length)
-  x <- dplyr::group_by(x, Subject, Trial, TimeBin)
+  x <- dplyr::group_by(x, Trial, TimeBin)
   x <- dplyr::mutate(x, Pupil_Diameter.mm = mean(Pupil_Diameter.mm, na.rm = TRUE))
   x <- dplyr::ungroup(x)
-  x <- dplyr::select(x, -TimeBin, -Message)
-  x <- dplyr::distinct(x)
+  x <- dplyr::select(x, -TimeBin)
+  x <- dplyr::distinct(x, Trial, Time, .keep_all = TRUE)
   return(x)
 }
