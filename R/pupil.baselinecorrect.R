34c6ab0fd9afcf5234cef33bfe5017acfa0d2f02
dr-JT <tsuky19@gmail.com>
2018-10-10 16:15:02 -0400

    add bc.type parameter

--- R/pupil.baselinecorrect.R
+++ R/pupil.baselinecorrect.R
@@ -4,12 +4,13 @@
 #' @param x dataframe
 #' @param baselineoffset.message Message string(s) that marks the offset of baseline period(s)
 #' @param bc.duration Duration baseline period(s) to use for correction
+#' @param bc.type Do you want to use "subtractive" or "divisive" baseline correction? (default: "subtractive")
 #' @keywords baseline
 #' @export
 #' @examples
 #' pupil.baselinecorrect(file = "path/filename", baseline.duration = 2000, start.trial = "# Message: Target")
 
-pupil.baselinecorrect <- function(x, baselineoffset.message = "", bc.duration = 200){
+pupil.baselinecorrect <- function(x, baselineoffset.message = "", bc.duration = 200, bc.type = "subtractive"){
   baselines.n <- length(baselineoffset.message)
   x <- dplyr::group_by(x, Trial)
   x <- dplyr::mutate(x, PreTarget=0, Target=0)
@@ -24,11 +25,16 @@ pupil.baselinecorrect <- function(x, baselineoffset.message = "", bc.duration = 
   }
   x <- dplyr::group_by(x, Trial, PreTarget)
   x <- dplyr::mutate(x,
-                     PreTarget.mean = ifelse(!is.na(PreTarget) & PreTarget>0, mean(Pupil_Diameter.mm, na.rm = TRUE), NA))
+                     PreTarget.mean = mean(Pupil_Diameter.mm, na.rm = TRUE),
+                     PreTarget.mean = ifelse(is.na(PreTarget) | PreTarget==0, NA, PreTarget.mean))
   x <- dplyr::group_by(x, Trial)
-  x <- dplyr::mutate(x,
-                     PreTarget.mean = zoo::na.locf(PreTarget.mean, na.rm = FALSE),
-                     Pupil_Diameter_bc.mm = ifelse(!is.na(Target), Pupil_Diameter.mm - PreTarget.mean, NA))
+  x <- dplyr::mutate(x, PreTarget.mean = zoo::na.locf(PreTarget.mean, na.rm = FALSE))
+  if (bc.type=="subtractive"){
+    x <- dplyr::mutate(x, Pupil_Diameter_bc.mm = Pupil_Diameter.mm - PreTarget.mean)
+  } else if (bc.type=="divisive"){
+    x <- dplyr::mutate(x, Pupil_Diameter_bc.mm = ((Pupil_Diameter.mm - PreTarget.mean)/PreTarget.mean)*100)
+  }
+
   x <- dplyr::ungroup(x)
   x <- dplyr::select(x, -PreTarget.mean, -baselineoffset.time)
   return(x)
