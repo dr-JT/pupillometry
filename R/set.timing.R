fe8ac59ce221f495f12022f99eaf1b9958fb2041
dr-JT <tsuky19@gmail.com>
2018-10-11 17:32:43 -0400

    fix set.timing

--- R/set.timing.R
+++ R/set.timing.R
@@ -18,6 +18,7 @@ set.timing <- function(x, trialonset.message = "", ms.conversion = 1, pretrial.d
                      min = min(trialonset.time, na.rm = TRUE),
                      trialonset.time = ifelse(is.na(trialonset.time) | trialonset.time!=min,NA,trialonset.time),
                      trialonset.time = zoo::na.locf(trialonset.time, na.rm = FALSE),
+                     trialonset.time = zoo::na.locf(trialonset.time, na.rm = FALSE, fromLast = TRUE),
                      Time = (Time - trialonset.time)/ms.conversion,
                      PreTrial = ifelse(Time>=pretrial.duration & Time<0,1,ifelse(Time>=0,0,NA)))
   x <- dplyr::ungroup(x)
