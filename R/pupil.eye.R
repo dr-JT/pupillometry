163eebeb4a60bdbe98bc17c40ddb9a273e4a0bef
dr-JT <tsuky19@gmail.com>
2018-10-12 13:44:54 -0400

    fix

--- R/pupil.eye.R
+++ R/pupil.eye.R
@@ -20,15 +20,17 @@ pupil.eye <- function(x, eye.recorded = "", eye.use = "", missing.allowed = 1){
     x <- pupil.cor(x)
     # remove either left or right eye
     if (eye.use=="left"){
-      x <- dplyr::rename(x, Pupil_Diameter.mm = L_Pupil_Diameter.mm,
-                            Missing.Total = L_Missing.Total,
-                            Eye_Event = L_Event)
+      x <- dplyr::rename(x,
+                         Pupil_Diameter.mm = L_Pupil_Diameter.mm,
+                         Missing.Total = L_Missing.Total,
+                         Eye_Event = L_Event)
+      x <- dplyr::select(x, -R_Pupil_Diameter.mm, -R_Missing.Total, -R_Event)
     } else if (eye.use=="right"){
-      x <- dplyr::rename(x, Pupil_Diameter.mm = R_Pupil_Diameter.mm,
-                            Missing.Total = R_Missing.Total, Eye_Event = R_Event)
+      x <- dplyr::rename(x,
+                         Pupil_Diameter.mm = R_Pupil_Diameter.mm,
+                         Missing.Total = R_Missing.Total, Eye_Event = R_Event)
+      x <- dplyr::select(x, -L_Pupil_Diameter.mm, -L_Missing.Total, -L_Event)
     }
-    x <- dplyr::select(x, -L_Pupil_Diameter.mm, -R_Pupil_Diameter.mm,
-                          -L_Missing.Total, -R_Missing.Total, -L_Event, -R_Event)
   } else if (eye.recorded=="left"){
     x <- dplyr::rename(x, Pupil_Diameter.mm = L_Pupil_Diameter.mm,
                           Missing.Total = L_Missing.Total,
