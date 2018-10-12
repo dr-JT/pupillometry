f64c830398abfc54719baa2a2d852a3cc557adda
dr-JT <tsuky19@gmail.com>
2018-10-12 14:57:10 -0400

    more flexibility

--- R/plot.comparison.R
+++ R/plot.comparison.R
@@ -2,48 +2,27 @@
 #'
 #' This function will print ggplot figures of na.removed, method.first = interpolate, and method.first = smooth
 #' @param import Directory with preprocessed data files for comparison
+#' @param files Vector of files to be compared
+#' @param labels Vector of labels for plotting.
 #' @param trial Which trial to filter on
 #' @keywords
 #' @export plot.comparison
 #' @examples
 
-plot.comparison <- function(import, trial){
-  filelist <- list.files(import, pattern = "PupilData")
-  i <- filelist[stringr::str_which(filelist, "_interpolated")]
-  s <- filelist[stringr::str_which(filelist, "_smoothed")]
-  na <- filelist[stringr::str_which(filelist, "naremoved")]
+plot.comparison <- function(import, files = c(), labels = c(), trial){
 
-  if (length(i)>0){
-    i <- readr::read_delim(paste(import, i, sep = "/"),
-                           delim = "\t", escape_double = FALSE, trim_ws = TRUE)
-    i <- dplyr::filter(i, Trial==trial)
-    i <- dplyr::mutate(i, First = "Interpolate")
-    i <- dplyr::select(i, Time, First, Pupil_Diameter.mm)
-  } else {
-    i <- data.frame(Time = NA, First = NA, Pupil_Diameter.mm = NA)
+  data <- list()
+  for (i in seq_along(files)){
+    data[[i]] <- readr::read_delim(paste(import, files[i], sep = "/"), delim = "\t",
+                                   escape_double = FALSE, trim_ws = TRUE)
+    data[[i]] <- dplyr::filter(data[[i]], Trial==trial)
+    data[[i]] <- dplyr::mutate(data[[i]], Labels = labels[i])
+    data[[i]] <- dplyr::select(data[[i]], Time, Labels, Pupil_Diameter.mm)
   }
-  if (length(s)>0){
-    s <- readr::read_delim(paste(import, s, sep = "/"),
-                           delim = "\t", escape_double = FALSE, trim_ws = TRUE)
-    s <- dplyr::filter(s, Trial==trial)
-    s <- dplyr::mutate(s, First = "Smooth")
-    s <- dplyr::select(s, Time, First, Pupil_Diameter.mm)
-  } else {
-    s <- data.frame(Time = NA, First = NA, Pupil_Diameter.mm = NA)
-  }
-  if (length(na)>0){
-    na <- readr::read_delim(paste(import, na, sep = "/"),
-                           delim = "\t", escape_double = FALSE, trim_ws = TRUE)
-    na <- dplyr::filter(na, Trial==trial)
-    na <- dplyr::mutate(na, First = "NA.Removed")
-    na <- dplyr::select(na, Time, First, Pupil_Diameter.mm)
-  } else {
-    na <- data.frame(Time = NA, First = NA, Pupil_Diameter.mm = NA)
-  }
+  data <- dplyr::bind_rows(data)
 
-  plot <- dplyr::bind_rows(list(i, s, na))
   plot <- dplyr::filter(plot, !is.na(Time))
-  plot <- ggplot2::ggplot(plot, aes(x = Time, y = Pupil_Diameter.mm, color = First)) +
-    geom_point(alpha = .35)
+  plot <- ggplot2::ggplot(plot, ggplot2::aes(x = Time, y = Pupil_Diameter.mm, color = Labels, shape = Labels)) +
+    ggplot2::geom_point(alpha = .35)
   return(plot)
 }
