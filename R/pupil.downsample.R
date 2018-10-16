081266d017aa4a2c7f5e441bbbf663fe1c40bc63
dr-JT <tsuky19@gmail.com>
2018-10-16 11:56:00 -0400

    fix

--- R/pupil.downsample.R
+++ R/pupil.downsample.R
@@ -12,8 +12,7 @@
 pupil.downsample <- function(x, bin.length = NULL, bc = FALSE){
   x <- dplyr::group_by(x, Trial)
   x <- dplyr::mutate(x,
-                     TimeBin = ifelse(min(Time)>=0, Time - min(Time), Time),
-                     TimeBin = trunc((Time - min(Time))/bin.length),
+                     TimeBin = trunc(Time/bin.length),
                      TimeBin = ifelse(Time<0, TimeBin - 1, TimeBin),
                      Time = TimeBin*bin.length)
   x <- dplyr::group_by(x, Trial, TimeBin)
