2057b651f583d6c93f14b3533a940053ef3a5cd3
Jason Tsukahara <tsuky19@gmail.com>
2018-11-13 13:49:55 -0500

    deprecate pupil.missing

--- R/pupil_missing.R
+++ R/pupil_missing.R
@@ -3,26 +3,16 @@
 #' This function is for preprocessing baseline pupil x. It takes as input a x file that has been processed
 #' with the .convert() functions. The output of this function is a preprocessed x file. Preprocessing steps can
 #' include interpolation and smoothing
-#' @param x xframe
+#' @param x dataframe
 #' @param missing.allowed What proportion of missing data is allowed, per trial?
 #' @param velocity The velocity threshold for Blink detection
 #' @param margin The margin before and after Blink onset and offset
-#'
-#' @templateVar fun pupil.missing
-#' @template template-depr_fun
-NULL
-
-#' @templateVar old pupil.missing
-#' @templateVar new pupil_missing
-#' @template template-depr_pkg
-#'
 #' @keywords preprocess
 #' @export
 #' @examples
-#' pupil.missing(x)
+#' pupil_missing(x)
 
-pupil.missing <- function(x, missing.allowed = 1, velocity = "", margin = ""){
-  .Deprecated("pupil_missing")
+pupil_missing <- function(x, missing.allowed = 1, velocity = "", margin = ""){
   x <- dplyr::group_by(x, Trial)
   x <- dplyr::mutate(x,
                      Pupil_Diameter.mm = ifelse(Event=="Blink"|is.na(Event)|Pupil_Diameter.mm==0, NA, Pupil_Diameter.mm),
