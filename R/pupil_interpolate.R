f351e0dbe0ede441cddec8c81a2bdb45d3e0180b
Jason Tsukahara <tsuky19@gmail.com>
2019-05-15 12:33:51 -0400

    fix

--- R/pupil_interpolate.R
+++ R/pupil_interpolate.R
@@ -19,16 +19,17 @@ pupil_interpolate <- function(x, type = "cubic-spline", maxgap = Inf, hz = ""){
   if (type=="cubic-spline"){
     x <- dplyr::mutate(x,
                        index = ifelse(is.na(Pupil_Diameter.mm), NA, dplyr::row_number()),
-                       no.data = ifelse(sum(index, na.rm = TRUE) == 0, 1, 0),
                        index = zoo::na.approx(index, na.rm = FALSE),
+                       Missing.Total = ifelse(is.na(Pupil_Diameter.mm), 1, 0),
+                       Missing.Total = sum(Missing.Total, na.rm = TRUE)/dplyr::n(),
                        Pupil_Diameter.mm =
-                         ifelse(no.data == 0,
+                         ifelse(Missing.Total < 1,
                                 zoo::na.spline(Pupil_Diameter.mm,
                                                na.rm = FALSE,
                                                x = index,
                                                maxgap = maxgap),
                                 Pupil_Diameter.mm))
-    x <- dplyr::select(x, -index, -no.data)
+    x <- dplyr::select(x, -index, -nodata)
   } else if (type=="linear"){
     x <- dplyr::mutate(x,
                        Pupil_Diameter.mm = zoo::na.approx(Pupil_Diameter.mm, na.rm = FALSE, maxgap = maxgap))
