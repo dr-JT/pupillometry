8ba10cba965361694231502d9546b0a625867a0d
dr-JT <tsuky19@gmail.com>
2019-05-16 14:20:41 -0400

    add = TRUE

--- R/pupil_downsample.R
+++ R/pupil_downsample.R
@@ -10,12 +10,12 @@
 #' pupil_downsample(x, bin.length = 100)
 
 pupil_downsample <- function(x, bin.length = NULL, bc = FALSE){
-  x <- dplyr::group_by(x, Trial)
+  x <- dplyr::group_by(x, Trial, add = TRUE)
   x <- dplyr::mutate(x,
                      TimeBin = trunc(Time/bin.length),
                      TimeBin = ifelse(Time<0, TimeBin - 1, TimeBin),
                      Time = TimeBin*bin.length)
-  x <- dplyr::group_by(x, Trial, TimeBin)
+  x <- dplyr::group_by(x, TimeBin, add = TRUE)
   x <- dplyr::mutate(x, Pupil_Diameter.mm = mean(Pupil_Diameter.mm, na.rm = TRUE))
   if (bc==TRUE){
     x <- dplyr::mutate(x, Pupil_Diameter_bc.mm = mean(Pupil_Diameter_bc.mm, na.rm = TRUE))
