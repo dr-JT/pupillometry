25b7f6f5a7b3e9e7f132377053c3965820dd3731
Tsukahara <jtsukahara3@psy-351-01.psych.gatech.edu>
2020-02-12 15:03:16 -0500

    update logo

---
+++ logo.R
@@ -0,0 +1,16 @@
+library(hexSticker)
+library(here)
+
+sticker(here("man/figures",
+             "white_eye.png"),
+        filename = here("man/figures", "logo.png"),
+        package = "pupillometry",
+        h_fill = "#104B7F",
+        h_color = "#043765",
+        p_size = 6,
+        s_width = .6,
+        s_x = 1.0,
+        s_y = .70)
+
+
+
