1d44370e91e6cd07e958b0631b3a70396c289249
Jason Tsukahara <tsuky19@gmail.com>
2020-05-24 10:53:00 -0400

    update

--- R/pupil_read.R
+++ R/pupil_read.R
@@ -346,7 +346,6 @@ pupil_read <- function(file, eyetracker = "", px_to_mm.conversion = NULL,
   check <- dplyr::group_by(check, Subject)
   check <- dplyr::summarise(check, Trials = n())
   check <- dplyr::ungroup(check)
-  check <- dplyr::distinct(check)
 
   if (!is.null(quality_check_dir)) {
     if (!dir.exists(quality_check_dir)) dir.create(quality_check_dir)
