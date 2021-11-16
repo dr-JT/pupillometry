b38c73b3d88727ad3a9803efd84152ccf2280e98
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 12:41:46 -0500

    update smooth function to be more useful

--- R/pupil_smooth.R
+++ R/pupil_smooth.R
@@ -1,17 +1,54 @@
-#' Apply smoothing/low frequency filter
+#' Apply a smoothing function on pupil data
 #'
-#' This function performs a smoothing function over an array of x
-#' The 'zoo' package is a dependency
+#' The purpose of smoothing is to reduce high-frequency
+#' fluctuations in the data (noise).
+#'
+#' @section Smoothing functions:
+#'
+#' This function applies either a hanning filter or
+#' n-point moving average smoothing function.
+#'
+#' The hanning filter uses the `dplR::hanning()` function.
+#' The n-point moving average uses the `zoo::rollapply(FUN = mean)` function.
+#'
+#' The window size (n) determines how much smoothing is applied, with a larger
+#' window size resulting in more smoothing. In general, it is advised not to
+#' use too large of a window size as this can result in diminishing meaningful
+#' signals in the data in addition to noise.
+#'
+#' The window size (n) needs to be specified in terms of the number of
+#' samples. When determining what window size to set, the sampling frequency
+#' of the pupil data needs to be taken into consideration. For example, a
+#' 5-point (n = 5) moving average at 250Hz is averaging over a 20 ms window,
+#' whereas a 5-point moving average at 1000Hz is averaging over a 5 ms window.
+#'
+#' @section Plot inspection:
+#'
+#' To inspect how the preprocesing step changed pupil size values,
+#' use `plot = TRUE`.
+#'
+#' Warning: this will create a separate plot for every trial and therefore can
+#' be time consuming and overwhelming. There is also the option to create
+#' aggregated plots over trial conditions with the `plot_aggregate` argument.
+#'
+#'
 #' @param x dataframe
 #' @param type The type of smoothing function to apply. "hann" or "mwa"
-#' @param n Number of samples to smooth over
-#'     (length of hanning filter for "hann" and window size for "mwa")
-#' @param window deprecated. See n
-#' @param hz deprecated. See n
+#' @param n The size of the smoothing window in samples
+#' @param plot Logical. Inspect a plot of how pupil values changed?
+#' @param plot_aggregate The name of the column to aggregate the plot by
+#' @param window Deprecated. Use n.
+#'     The size of the smoothing window in milliseconds
+#' @param hz Deprecated. Use n. The recording frequency.
+#'     Needed if specified a window size in milliseconds
 #' @export
 #'
+#'
 
-pupil_smooth <- function(x, type = "hann", n = NULL, window = NULL, hz = NULL){
+pupil_smooth <- function(x, type = "hann", n = NULL,
+                         plot = FALSE, plot_aggregate = NULL,
+                         window = NULL, hz = NULL){
+  x_before <- x
 
   real_name <- ifelse("Pupil_Diameter.mm" %in% colnames(x),
                       "Pupil_Diameter.mm", "Pupil_Diameter.px")
@@ -20,55 +57,34 @@ pupil_smooth <- function(x, type = "hann", n = NULL, window = NULL, hz = NULL){
 
   colnames(x)[which(colnames(x) == real_name)] <- "pupil_val"
 
-  if (!is.null(n)) {
-    if (type == "hann") {
-      x <- dplyr::mutate(x,
-                         hold = dplR::hanning(pupil_val, n = n),
-                         hold = zoo::na.approx(hold, rule=2),
-                         pupil_val = ifelse(is.na(pupil_val), NA, hold))
+  if (!is.null(window)) {
+    n <- round(window / (1000 / hz))
+    if (!(n %% 2)) {
+      n - 1
     }
-
-    if (type == "mwa") {
-      x <- dplyr::mutate(x,
-                         hold = zoo::rollapply(pupil_val,
-                                                    width = n,
-                                                    FUN = mean,
-                                                    partial = TRUE,
-                                                    na.rm = TRUE),
-                         hold = zoo::na.approx(hold, rule=2),
-                         pupil_val = ifelse(is.na(pupil_val), NA, hold))
-    }
-
-  } else if (!is.null(window)) {
-    window <- round(window / (1000 / hz))
-    if (type == "hann"){
-      x <- dplyr::mutate(x,
-                         hold = zoo::na.approx(pupil_val, na.rm = FALSE,
-                                               maxgap = Inf),
-                         hold = dplR::hanning(hold, n = window),
-                         pupil_val = ifelse(is.na(hold) &
-                                              !is.na(pupil_val),
-                                            pupil_val,
-                                            ifelse(is.na(pupil_val),
-                                                   NA, hold)))
-      x <- dplyr::select(x, -hold)
-    } else if (type == "mwa"){
-      x <- dplyr::mutate(x,
-                         hold = zoo::rollapply(pupil_val,
-                                               width = window,
-                                               FUN = mean,
-                                               partial = TRUE,
-                                               na.rm = TRUE),
-                         pupil_val = ifelse(is.na(pupil_val), NA, hold))
-      x <- dplyr::select(x, -hold)
-    }
-    x <- dplyr::mutate(x, Trial_missing.prop = ifelse(is.na(pupil_val), 1, 0),
-                       Trial_missing.prop =
-                         sum(Trial_missing.prop, na.rm = TRUE) / dplyr::n())
-    x <- dplyr::ungroup(x)
-    x <- dplyr::filter(x, Trial_missing.prop < 1)
-    x <- dplyr::select(x, -Trial_missing.prop)
   }
+
+  if (type == "hann") {
+    x <- dplyr::mutate(x,
+                       hold = dplR::hanning(pupil_val, n = n),
+                       hold = zoo::na.approx(hold, rule = 2),
+                       pupil_val = ifelse(is.na(pupil_val), NA, hold))
+  }
+  if (type == "mwa") {
+    x <- dplyr::mutate(x,
+                       hold = zoo::rollapply(pupil_val,
+                                             width = n,
+                                             FUN = mean,
+                                             na.rm = TRUE,
+                                             partial = TRUE),
+                       hold = zoo::na.approx(hold, rule = 2),
+                       pupil_val = ifelse(is.na(pupil_val), NA, hold))
+  }
+  x <- dplyr::select(x, -hold)
+  x <- dplyr::ungroup(x)
   colnames(x)[which(colnames(x) == "pupil_val")] <- real_name
+
+  if (plot == TRUE) pupil_plot(x_before, x, aggregate = plot_aggregate)
+
   return(x)
 }
