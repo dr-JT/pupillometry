df7eb77831534845fb3c17ae83b24ffdcd4a87d6
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 16:37:02 -0500

    Keep missing_trial column

--- R/pupil_missing.R
+++ R/pupil_missing.R
@@ -14,12 +14,11 @@ pupil_missing <- function(x, missing_allowed = 1, velocity = "", margin = ""){
   colnames(x)[which(colnames(x) == real_name)] <- "pupil_val"
   x <- dplyr::group_by(x, Trial)
   x <- dplyr::mutate(x,
-                     Missing_Trial = ifelse(is.na(pupil_val), 1, 0),
-                     Missing_Trial =
-                       sum(Missing_Trial, na.rm = TRUE) / dplyr::n())
+                     Pupil_Missing = ifelse(is.na(pupil_val), 1, 0),
+                     Pupil_Missing =
+                       sum(Pupil_Missing, na.rm = TRUE) / dplyr::n())
   x <- dplyr::ungroup(x)
-  x <- dplyr::filter(x, Missing_Trial <= missing_allowed)
-  x <- dplyr::select(x, -Missing_Trial)
+  x <- dplyr::filter(x, Pupil_Missing <= missing_allowed)
   colnames(x)[which(colnames(x) == "pupil_val")] <- real_name
 
   return(x)
