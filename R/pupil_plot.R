7a794f88d2c248ebd99ea023726bbd34188c9722
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 12:41:34 -0500

    plot before and after

---
+++ R/pupil_plot.R
@@ -0,0 +1,54 @@
+#' Plot and inspect how preprocessing steps are changing the pupil data.
+#' An exploratory function only.
+#'
+#' This function will print ggplot figures of either trial level or aggregated pupil data
+#' @param x dataframe
+#' @param aggregate column name to aggregate the plot by.
+#'     For individual trial plots leave as NULL.
+#' @export pupil_plot
+#'
+
+pupil_plot <- function(x, y, aggregate = NULL) {
+  real_name <- dplyr::case_when("Pupil_Diameter.mm" %in% colnames(x) ~
+                                  "Pupil_Diammeter.mm",
+                                "Pupil_Diameter.px" %in% colnames(x) ~
+                                  "Pupil_Diameter.px")
+  colnames(x)[which(colnames(x) == real_name)] <- "pupil_val_before"
+  colnames(y)[which(colnames(y) == real_name)] <- "pupil_val_after"
+  x <- dplyr::select(x, Trial, Time, pupil_val_before, dplyr::any_of(aggregate))
+  y <- dplyr::select(y, Trial, Time, pupil_val_after, dplyr::any_of(aggregate))
+  data_plot <- merge(x, y, by = c("Trial", "Time"), all = TRUE)
+
+  if (is.null(aggregate)) {
+    for (trial in unique(x$Trial)) {
+      data_trial <- dplyr::filter(data_plot, Trial == trial)
+      plot <- ggplot2::ggplot(data_trial, ggplot2::aes(Time)) +
+        ggplot2::geom_line(ggplot2::aes(y = pupil_val_before), alpha = .35) +
+        ggplot2::geom_line(ggplot2::aes(y = pupil_val_after)) +
+        ggplot2::ggtitle(paste("Trial: ", x$Trial[1], sep = "")) +
+        ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+        ggplot2::labs(y = "Pupil Size", x = "Time (ms)")
+      # Print plot
+      grid::grid.newpage()
+      grid::grid.draw(plot)
+    }
+  } else {
+    for (group in unique(x[[aggregate]])) {
+      x_group <- dplyr::filter(x, get(aggregate) == group)
+      plot <- ggplot2::ggplot(x_group,
+                              ggplot2::aes(Time, group = get(aggregate))) +
+        ggplot2::stat_summary(ggplot2::aes(y = pupil_val_before),
+                              fun = mean, na.rm = TRUE,
+                              geom = "line", alpha = .35) +
+        ggplot2::stat_summary(ggplot2::aes(y = pupil_val_after),
+                              fun = mean, na.rm = TRUE,
+                              geom = "line") +
+        ggplot2::ggtitle(paste("Group: ", x[1,aggregate], sep = "")) +
+        ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5)) +
+        ggplot2::labs(y = real_name, x = "Time (ms)")
+      # Print plot
+      grid::grid.newpage()
+      grid::grid.draw(plot)
+    }
+  }
+}
