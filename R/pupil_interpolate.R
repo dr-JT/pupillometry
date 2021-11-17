5255d66f0e2844adecfc3f3fdfd5c46b1fe5c804
dr-JT <jtsukahara3@gatech.edu>
2021-11-17 00:11:19 -0500

    arrange

--- R/pupil_interpolate.R
+++ R/pupil_interpolate.R
@@ -43,7 +43,7 @@ pupil_interpolate <- function(x, type = "cubic-spline",
                        pupil_val = zoo::na.approx(pupil_val, na.rm = FALSE,
                                                   maxgap = maxgap))
   }
-  x <- dplyr::arrange(x, Time)
+  x <- dplyr::arrange(x, Trial, Time)
   x <- dplyr::ungroup(x)
   colnames(x)[which(colnames(x) == "pupil_val")] <- real_name
 
