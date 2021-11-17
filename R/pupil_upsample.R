2b0133a206211febae99872c4ebe060e63871cf6
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 22:22:46 -0500

    fix

--- R/pupil_upsample.R
+++ R/pupil_upsample.R
@@ -8,14 +8,15 @@
 pupil_upsample <- function(x){
   for (trial in unique(x$Trial)) {
     x_trial <- dplyr::filter(x, Trial == trial)
-    time_up <- data.frame(Time = min(x_trial$Time):max(x_trial$Time))
-    x <- merge(x, time_up, by = "Time", all = TRUE)
-    x <- dplyr::relocate(x, Time, .after = "Trial")
-    x <- dplyr::arrange(x, Subject, Trial, Time)
-    x <- tidyr::fill(x,
-                     -tidyselect::any_of(c("Pupil_Diameter.mm", "Pupil_Diameter.px",
-                                    "Gaze_Position.x", "Gaze_Position.y")),
-                     .direction = "down")
+    time_up <- data.frame(Trial = trial,
+                          Time = min(x_trial$Time):max(x_trial$Time))
+    x <- merge(x, time_up, by = c("Trial", "Time"), all = TRUE)
   }
+  x <- dplyr::relocate(x, Subject, .before = "Trial")
+  x <- dplyr::arrange(x, Subject, Trial, Time)
+  x <- tidyr::fill(x,
+                   -tidyselect::any_of(c("Pupil_Diameter.mm", "Pupil_Diameter.px",
+                                         "Gaze_Position.x", "Gaze_Position.y")),
+                   .direction = "down")
   return(x)
 }
