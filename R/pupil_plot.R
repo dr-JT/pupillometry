e6a9bd96897bbea824ceba4869bb8317f1a2416a
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 20:44:03 -0500

    fix aggregate

--- R/pupil_plot.R
+++ R/pupil_plot.R
@@ -37,10 +37,10 @@ pupil_plot <- function(x, y, aggregate = NULL) {
       data_group <- dplyr::filter(data_plot, aggregate == group)
       plot <- ggplot2::ggplot(data_group,
                               ggplot2::aes(Time)) +
-        ggplot2::stat_summary(ggplot2::aes(y = pupil_val_before),
+        ggplot2::stat_summary(ggplot2::aes(y = pupil_val_before, group = 1),
                               fun = mean, na.rm = TRUE,
                               geom = "line", alpha = .35) +
-        ggplot2::stat_summary(ggplot2::aes(y = pupil_val_after),
+        ggplot2::stat_summary(ggplot2::aes(y = pupil_val_after, group = 1),
                               fun = mean, na.rm = TRUE,
                               geom = "line") +
         ggplot2::ggtitle(paste("Group: ", data_group[1,aggregate], sep = "")) +
