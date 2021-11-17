254e1e653109029f0784d1f938c377df183b7578
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 21:58:24 -0500

    add upsample to 1000 hz function

---
+++ R/pupil_upsample.R
@@ -0,0 +1,15 @@
+#' Upsample pupil data
+#'
+#' This function will increase the sampling requency to 1000Hz
+#' @param x dataframe
+#' @export
+#'
+
+pupil_upsample <- function(x){
+  for (trial in unique(x$Trial)) {
+    x_trial <- dplyr::filter(x, Trial == trial)
+    time_up <- data.frame(Time = min(x_trial$Time):max(x_trial$Time))
+    x <- merge(x, time_up, by = "Time", all = TRUE)
+  }
+  return(x)
+}
