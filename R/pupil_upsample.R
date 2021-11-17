a68a4130163912acc7e9571f2322ff274d35c571
dr-JT <jtsukahara3@gatech.edu>
2021-11-16 22:02:37 -0500

    arrange and reorder columns

--- R/pupil_upsample.R
+++ R/pupil_upsample.R
@@ -10,6 +10,8 @@ pupil_upsample <- function(x){
     x_trial <- dplyr::filter(x, Trial == trial)
     time_up <- data.frame(Time = min(x_trial$Time):max(x_trial$Time))
     x <- merge(x, time_up, by = "Time", all = TRUE)
+    x <- dplyr::arrange(x, Subject, Time, Trial)
+    x <- dplyr::relocate(x, Time, .after = "Trial")
   }
   return(x)
 }
