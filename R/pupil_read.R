a6b2eb27425aa4eb2fbd3957bee3e2b04cf5cd0a
Tsukahara <jtsukahara3@dhcp-128-61-154-155.psych.gatech.edu>
2022-07-20 16:37:22 -0400

    fix

--- R/pupil_read.R
+++ R/pupil_read.R
@@ -555,6 +555,8 @@ pupil_read <- function(file, eyetracker = "", eye_use = NULL,
   ## Eye tracker is Tobii ####
   if (eyetracker == "tobii") {
 
+    start_tracking_message <- NULL
+
     data <- readr::read_delim(file, "\t", escape_double = FALSE,
                               trim_ws = TRUE, na = ".", guess_max = 100000)
 
